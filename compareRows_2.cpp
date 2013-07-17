626bd92397bab5cb6779748e9455fb82a483d9f1
rrfuentes <r.fuentes@irri.org>
2013-07-17 14:30:46 +0800

    Corrected compareRows_2

--- compareRows_2.cpp
+++ compareRows_2.cpp
@@ -1,7 +1,7 @@
 /*
  *version: 2.0
  * Load the specified row/s in memory and compare(serial/pthread) against a reference row.
- * Last Updated:June 26, 2013
+ * Last Updated:July 17, 2013
  * Works on data > memorysize 
  *
  *Author: Roven Rommel B. Fuentes
@@ -25,7 +25,7 @@ using namespace std;
 
 static const char *options="f:F:p:P:n:N:r:R:o:O:c:C";
 static string pos;
-static string colrange;
+static string snpbound;
 static string datafile;
 static string varPath;
 static string varName;
@@ -38,7 +38,7 @@ struct threadData {
     int* data;
     int* refdata;
     int* consensus;
-    int columnlen;
+    int snpcount;
     int blocksize;
     int tid;
     int tcount;
@@ -58,7 +58,7 @@ void parseArgs(int argc, char **argv){
 	    case 'r':
 	    case 'R': pos = optarg; break; //e.g: 'x|y:z' where x=ref sample, y=sample for comparison, y:z=range
 	    case 'c':
-	    case 'C': colrange = optarg; break; //e.g: x:y, where x=start, y=end
+	    case 'C': snpbound = optarg; break; //e.g: x:y, where x=start, y=end
 	    case 'o':
 	    case 'O': outfile = optarg; break; 
 	    default: break;
@@ -68,44 +68,27 @@ void parseArgs(int argc, char **argv){
 
 }
 
-void *compareRow(void* thread){
+void *compareSample(void* thread){
     threadData *t = (threadData*) thread;
-    int columnlen = t->columnlen;
+    int snpcount = t->snpcount;
     int blocksize = t->blocksize;
     int tid = t->tid;
     int start=0, end=0,idxrel=0,temp=0;
-    if(columnlen>blocksize){ /*VERTICAL CHOPPING of data*/
-	int skip=columnlen/t->tcount; /*columns per chunk*/
-		
-	for(int x=0;x<blocksize;x++){
-	    start=(x*columnlen)+(tid*skip); //printf("thread:%d;start:%d\n",tid,start);
-	    end=start+skip; 
-	    if(columnlen%t->tcount>0 && tid==t->tcount-1) /*Last chunk is larger*/
-		end=start+ skip + (columnlen%t->tcount);
-				
-	    for(int i=start; i<end;i++){
-		    idxrel=i%columnlen;
-		    t->consensus[idxrel]=t->consensus[idxrel]||(t->data[i]!=t->refdata[idxrel]);
-		    t->data[i]=(t->data[i]!=t->refdata[idxrel])?t->data[i]:25;
-	    }
-	}
-    }else{ /*HORIZONTAL CHOPPING of data*/
-	int rows=blocksize/t->tcount;
-	int start=tid*rows;
-	int end=start+rows;
-	if(blocksize%t->tcount>0 && tid==t->tcount-1) /*Last chunk is larger*/
-	    end=start+rows+(blocksize%t->tcount);
-	for(int x=start;x<end;x++){
-	    int endpos = (x+1)*columnlen;
-	    for(int i=x*columnlen;i<endpos;i++){
-		idxrel=i%columnlen;
-		t->consensus[idxrel]=t->consensus[idxrel] || (t->data[i]!=t->refdata[idxrel]);
-		t->data[i]=(t->data[i]!=t->refdata[idxrel])?t->data[i]:25;
-	    }
+    /*VERTICAL CHOPPING of data*/
+    int skip=snpcount/t->tcount; /*rows/snps per chunk*/
+    if(snpcount%t->tcount>0 && tid==t->tcount-1) /*Last chunk is larger*/
+		skip += (snpcount%t->tcount);
+
+    for(int x=0;x<skip;x++){
+	start=(tid*skip*blocksize)+(blocksize*x); 
+	end=start+blocksize; 
+	idxrel=(tid*skip)+x; /*snp index*/			
+	for(int i=start; i<end;i++){ /*compare samples*/
+	    t->consensus[idxrel]=t->consensus[idxrel]||(t->data[i]!=t->refdata[idxrel]);
+	    t->data[i]=(t->data[i]!=t->refdata[idxrel])?t->data[i]:25;
 	}
     }
-	
-	return 0;
+    return 0;
 }
 
 int main(int argc, char **argv) {
@@ -155,7 +138,7 @@ int main(int argc, char **argv) {
 		  << " -n variable-name"
                   << " -p variable-path"
 		  << " -r ref&row indices ('x|y:z' where x=ref sample,x:y as row range) "
-		  << " -r column range (x:y, where x=start, y=end)"
+		  << " -r snp bounds (x:y, where x=start, y=end)"
 		  << " -d variable-dimension (e.g. 2:2)"
 		  << " -o output-file"
                   << std::endl;
@@ -190,7 +173,7 @@ int main(int argc, char **argv) {
 	    	
 	string str,param; 
 	vector<uint64_t> sample;
-	vector<uint64_t> col;
+	vector<uint64_t> row; //snps
 	int prePos = 0, idx = 0,blocksize=1;
 	FastQuery* fq = new FastQuery(datafile, model, "", 0, "",""); 
 	int *refdata=NULL, *data=NULL, *consensus=NULL;
@@ -213,7 +196,7 @@ int main(int argc, char **argv) {
 		prePos=idx+1;
 		str = pos.substr(prePos,pos.length() - prePos); 
 		sample.push_back(atoi(str.c_str())); /*get the end index*/
-		if(sample[2]<0 || sample[2]>=dims[0] || sample[1]>=sample[2]){
+		if(sample[2]<0 || sample[2]>=dims[1] || sample[1]>=sample[2]){
 		    printf("ERROR: Indices out of bounds/invalid range.\n");
 		    return 0;
 		}
@@ -222,7 +205,7 @@ int main(int argc, char **argv) {
 		sample.push_back(atoi(str.c_str())); /*get the index of another sample*/	
 	    }
 
-	    if(sample[0]<0 || sample[0]>=dims[0] || sample[1]<0 || sample[1]>=dims[0]){
+	    if(sample[0]<0 || sample[0]>=dims[1] || sample[1]<0 || sample[1]>=dims[1]){
 		printf("ERROR: Indices out of bounds.\n");
 		return 0;
 	    }
@@ -231,29 +214,29 @@ int main(int argc, char **argv) {
 	    return 0;
 	}
 
-	/*Get the column range for each sample*/
-	if(!colrange.empty()){
-	    idx=colrange.find(':',0);
-	    if(idx==colrange.npos || idx==colrange.length()-1){
-		printf("ERROR: Invalid column range.");
+	/*Get the SNP bounds for subset sample*/
+	if(!snpbound.empty()){
+	    idx=snpbound.find(':',0);
+	    if(idx==snpbound.npos || idx==snpbound.length()-1){
+		printf("ERROR: Invalid SNP bounds.");
 		return 1;
 	    }
-	    str = colrange.substr(0,idx);
-	    col.push_back(atoi(str.c_str()));
-	    str= colrange.substr(idx+1,colrange.length()-idx+1);
-	    col.push_back(atoi(str.c_str()));
-	    if(col[0]<0 || col[1]>=dims[1] || col[1]<0 || col[0]>=col[1]){
-	    	printf("ERROR: Invalid column range.\n");
+	    str = snpbound.substr(0,idx);
+	    row.push_back(atoi(str.c_str()));
+	    str= snpbound.substr(idx+1,snpbound.length()-idx+1);
+	    row.push_back(atoi(str.c_str()));
+	    if(row[0]<0 || row[1]>=dims[0] || row[1]<0 || row[0]>=row[1]){
+	    	printf("ERROR: Invalid SNP bounds.\n");
 	        return 0;
 	    }
-	    dims[1]=col[1]-col[0]+1; /*dims is now the col range for subregion*/
+	    dims[0]=row[1]-row[0]+1; /*dims is now the SNP bounds for subregion*/
 	}
 
 	if(sample.size()==3){
 	    blocksize=sample[2]-sample[1]+1; /*block of data*/
 	}
 	
-        if((dims[1]+(dims[1]*blocksize)+dims[1])*sizeof(int)>(1000*1024*1024)){ 
+        if((dims[0]+(dims[0]*blocksize)+dims[0])*sizeof(int)>(1000*1024*1024)){ 
 	    //printf("Error: Insufficient memory to handle huge block.\nREPORT: Failed to complete comparing data.\n");
 	    //return 1;
 	    printf("\nData is greater than the available/alloted memory space.\n");
@@ -261,14 +244,14 @@ int main(int argc, char **argv) {
      	}
 
         printf("Running with %d thread/s.\n",NUMTHREADS);
-        int colchunk=dims[1]/CHOP, offset; /*CHOP is 1 if data fits in memory*/
+        int rowchunk=dims[0]/CHOP, offset; /*CHOP is 1 if data fits in memory*/
 	float com_time=0;
 	ostringstream outtext;
 	pthread_t threads[NUMTHREADS];
 	threadData *thread_data = (threadData*)malloc(NUMTHREADS*sizeof(threadData));
-        refdata=(int*)malloc(colchunk*sizeof(int)); 
-	data=(int*)malloc((colchunk*blocksize)*sizeof(int));
-	consensus=(int*)calloc(colchunk,sizeof(int));
+        refdata=(int*)malloc(rowchunk*sizeof(int)); 
+	data=(int*)malloc((rowchunk*blocksize)*sizeof(int));
+	consensus=(int*)calloc(rowchunk,sizeof(int));
         output=fopen(outfile.c_str(),"w");
         
         /*Print output header*/
@@ -282,54 +265,55 @@ int main(int argc, char **argv) {
 	}
 	fprintf(output,"\n");
 
-	if(!colrange.empty()){ 
-	    offset=(int)col[0];
+	if(!snpbound.empty()){  /*set start position if bounded*/
+	    offset=(int)row[0];
 	}
 	int rem=0;
-        if(dims[1]%CHOP!=0){
-            rem = dims[1]%CHOP;
+        if(dims[0]%CHOP!=0){
+            rem = dims[0]%CHOP;
 	    CHOP++; /*another chunk for the remainder*/
 	}
+
         for(int h=0;h<CHOP;h++){
 	    if(h+1==CHOP && rem!=0){
-		colchunk=rem;
+		rowchunk=rem;
 		free(data);
 		free(refdata);
 		free(consensus);
-		data=(int*)malloc((colchunk*blocksize)*sizeof(int));
-		refdata=(int*)malloc(colchunk*sizeof(int));
-		consensus=(int*)calloc(colchunk,sizeof(int));
-		printf("Remainder chunk.%d\n",colchunk);
+		data=(int*)malloc((rowchunk*blocksize)*sizeof(int));
+		refdata=(int*)malloc(rowchunk*sizeof(int));
+		consensus=(int*)calloc(rowchunk,sizeof(int));
+		printf("Remainder chunk.%d\n",rowchunk);
 	    }
             
-	    if(!colrange.empty()){
-		ref << variable << "[" << sample[0] << ","<< offset <<":"<< offset+colchunk <<"]";
+	    if(!snpbound.empty()){
+		ref << variable << "[" << offset <<":"<< offset+rowchunk << "," <<  sample[0] << "]";
 		if(sample.size()==3)
-		    paramtemp << variable << "[" << sample[1] << ":" << sample[2]+1 << ","<< offset <<":"<< offset+colchunk <<"]"; 
+		    paramtemp << variable << "[" << offset <<":"<< offset+rowchunk << "," << sample[1] << ":" << sample[2]+1  <<"]"; 
 		else
-		    paramtemp << variable << "[" << sample[1] << ","<< offset <<":"<< offset+colchunk <<"]";
+		    paramtemp << variable << "[" << offset <<":"<< offset+rowchunk << "," << sample[1] << "]";
 	    }else{
-		ref << variable << "[" << sample[0] << ",:]";
+		ref << variable << "[:," << sample[0] << "]";
 		if(sample.size()==3)
-		    paramtemp << variable << "[" << sample[1] << ":" << sample[2]+1 << ",:]";
+		    paramtemp << variable << "[:," << sample[1] << ":" << sample[2]+1 << "]";
 		else
-		    paramtemp << variable << "[" << sample[1] << ",:]";
+		    paramtemp << variable << "[:," << sample[1] << "]";
 	    }
 	
 	    param = ref.str(); 
 	    fq->getData(param,refdata); 
 	    param = paramtemp.str();  
 	    cout << h << "Ref:"<< ref.str() <<"\tParam:"<<paramtemp.str()<<"\n\n";
-	    fq->getData(param,data); /*param=var[0:2,:]*/
+	    fq->getData(param,data); /*param=var[:,0:2]*/
 
             /*printf("Reference Row:\n");
-	    for(int i=0; icolchunk;i++) printf("%d ",refdata[i]);
+	    for(int i=0; i<rowchunk;i++) printf("%d ",refdata[i]);
 	    printf("\n\n");
 
 	    printf("Comparison Row Block:\n");
-	    for(int x=0;x<(blocksize*colchunk);x++){
+	    for(int x=0;x<(blocksize*rowchunk);x++){
 	    	printf("%d ",data[x]); 
-	    	if((x+1)%colchunk==0) printf("\n\n");
+	    	if((x+1)%blocksize==0) printf("\n\n");
 	    }*/
 
             /*THREADING of the comparison*/	
@@ -337,11 +321,11 @@ int main(int argc, char **argv) {
 	    	thread_data[i].refdata = refdata;
 	    	thread_data[i].data = data;
 	    	thread_data[i].consensus=consensus; 
-	    	thread_data[i].columnlen = colchunk;
+	    	thread_data[i].snpcount = rowchunk;
 	    	thread_data[i].blocksize = blocksize;
 	    	thread_data[i].tid = i;
 	    	thread_data[i].tcount = NUMTHREADS;
-	    	pthread_create(&threads[i],NULL,compareRow, (void*) &thread_data[i]);
+	    	pthread_create(&threads[i],NULL,compareSample, (void*) &thread_data[i]);
 	    }
 
 	    for(int i=0;i<NUMTHREADS;i++){
@@ -350,18 +334,18 @@ int main(int argc, char **argv) {
 
             /*RESULT printing*/
             timer1.stop();
-	    for(int i=0;i<colchunk;i++){
+	    for(int i=0;i<rowchunk;i++){
 	    	if(consensus[i]==1){
 		    outtext << i+offset << "\t" << Calls[refdata[i]] << "\t";
 		    for(int x=0;x<blocksize;x++){
-		    	outtext << Calls[data[x*colchunk+i]] << "\t"; 
+		    	outtext << Calls[data[i*blocksize+x]] << "\t"; 
 		    }
 		    fprintf(output,"%s\n",outtext.str().c_str());
 		    outtext.str("");
 	    	}
 			
 	    }
- 	    offset+=colchunk;
+ 	    offset+=rowchunk;
 	    paramtemp.str("");
 	    ref.str("");
             outtext.str("");
