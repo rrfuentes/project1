85610911c0f3400c466641449513e48ccf002032
rrfuentes <r.fuentes@irri.org>
2013-07-29 17:15:18 +0800

    update

--- writeVCF.cpp
+++ writeVCF.cpp
@@ -19,6 +19,7 @@
 
 #define SIZE1 20
 #define SIZE2 20
+#define SIZE3 20
 using namespace std;
 
 static const char *options="f:F:p:P:n:N:i:I:c:C:r:R:";
@@ -41,6 +42,10 @@ typedef struct{
     char id[SIZE2];
     int len;
 }META_2;
+
+typedef struct{
+   char name[SIZE3];
+}SamNames;
 
 /*INFO&FORMAT IDs*/
 typedef struct{
@@ -107,14 +112,15 @@ void parseHeader1(string linestream, map<string,string> &headmap){
     }
 }
 
-void loadHeader1(map<string,string> &headmap,META_1 *header,hid_t file){
+void loadHeader1(map<string,string> &headmap,hid_t file){
     /*relocate header to META_1 before loading to HDF5*/
-    int x=0,count=headmap.size();
+    int x=0,count=headmap.size(); 
+    META_1 header[count];
     hid_t memtype,space,dset;
     hid_t t1,t2;
     hsize_t dim[1]={count};
     herr_t status;
-    string name = varName + "metainfo";
+    string name = varPath + varName + "_meta";
     for (map<string,string>::iterator it=headmap.begin(); it!=headmap.end();++it){
 	strcpy(header[x].field, it->first.c_str());
 	header[x].value = (char*)malloc(it->second.length()*sizeof(char)+1);
@@ -145,23 +151,62 @@ void loadHeader1(map<string,string> &headmap,META_1 *header,hid_t file){
     }
 }
 
-void parseHeader2(string linestream,META_2 *header,int count){
-    int idx1 = 0, idx2 = 0;
-    header = (META_2*)realloc(header,count*sizeof(META_2));
-    idx1 = linestream.find("ID")+3;
-    idx2 = linestream.find_first_of(",",idx1);
-    strcpy(header[count-1].id,(linestream.substr(idx1,idx2-idx1)).c_str());
-    idx1 = linestream.find_first_of("length",idx2)+7; 
-    idx2 = linestream.find_first_of(">",idx1);
-    header[count-1].len=atoi((linestream.substr(idx1,idx2-idx1)).c_str());
-    cout << header[count-1].id << "==>" << header[count-1].len << "\n";
+void loadHeader2(META_2 *contig,hid_t file,int count){
+    hid_t memtype,space,dset,t1;
+    hsize_t dim[1]={count};
+    herr_t status;
+    string name= varPath + varName + "_contigs";
+    space = H5Screate_simple(1,dim,NULL);
+    t1 = H5Tcopy(H5T_C_S1);
+    H5Tset_size(t1,SIZE2);
+    memtype = H5Tcreate(H5T_COMPOUND,sizeof(META_2));
+    H5Tinsert(memtype,"id",HOFFSET(META_2,id),t1);
+    H5Tinsert(memtype,"len",HOFFSET(META_2,len),H5T_NATIVE_INT);
+    dset = H5Dcreate (file, name.c_str(), memtype, space, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    assert(status >=0);
+    status = H5Dwrite(dset, memtype, H5S_ALL, H5S_ALL, H5P_DEFAULT, contig);
+   
+    assert(status >=0);
+    /*free value pointers and variables*/
+    status = H5Dclose(dset);
+    status = H5Sclose(space);
+    status = H5Tclose(memtype);
+    assert(status>=0);
+    free(contig);
 }
 
+void parseHeader2(string linestream,META_2 *&contig,int count){
+    int idx1 = 0, idx2 = 0;
+    contig = (META_2*)realloc(contig,count*sizeof(META_2));
+    idx1 = linestream.find("ID")+3;
+    idx2 = linestream.find_first_of(",",idx1);
+    strcpy(contig[count-1].id,(linestream.substr(idx1,idx2-idx1)).c_str());
+    idx1 = linestream.find_first_of("length",idx2)+7; 
+    idx2 = linestream.find_first_of(">",idx1);
+    contig[count-1].len=atoi((linestream.substr(idx1,idx2-idx1)).c_str());
+    //cout << contig[count-1].id << "==>" << contig[count-1].len << "\n";
+}
+
+
+void loadSampleNames(string linestream, hid_t file){
+    SamNames *samples;
+    int idx1=0,idx2=0,temp=0;
+    for(int x=0;x<9;x++){ //ignore fixed table header
+        idx1 = linestream.find_first_of("\t",idx1+1); 
+    } cout<<"\n";
+    for(int x=0;idx1!=linestream.npos;x++){
+	//samples = (SamNames*)realloc(samples,(x+1)*sizeof(SamNames));
+        idx2 = linestream.find_first_of("\t",idx1+1); cout << idx2 << " ";
+        //strcpy(samples[x].name,(linestream.substr(idx1+1,idx2-idx1)).c_str());
+        idx1=idx2; 
+        //cout << samples[x].name << "\n";
+    }
+}
 int main(int argc, char **argv){
     const rlim_t STACK_SIZE = 1000*1024*1024; 
     struct rlimit rl;
-    rl.rlim_cur = STACK_SIZE;
-    int ret = setrlimit(RLIMIT_STACK,&rl);
+    //rl.rlim_cur = STACK_SIZE;
+    //int ret = setrlimit(RLIMIT_STACK,&rl);
 
     parseArgs(argc, argv);
     if (datafile.empty() || inputfile.empty() || varName.empty()
@@ -185,30 +230,29 @@ int main(int argc, char **argv){
     }
 
     char* line=NULL;
-    map<string,string> headmap1;
+    map<string,string> headmap;
     string linestream;
     hid_t file;
-    META_1* header1=NULL;
-    META_2* header2=NULL;
+    META_2* contig=NULL;
     int contigcount=0;
     /*create new file*/
     file = H5Fcreate(datafile.c_str(),H5F_ACC_TRUNC,H5P_DEFAULT,H5P_DEFAULT); 
     for(int i=0;getline(fp,linestream);i++){
-        if(linestream.substr(0,2) != "##"){ 
-	    continue; /*finished reading header*/
-	}else if(linestream.substr(0,8) == "##contig"){
-	    //parseHeader2(linestream,header2,++contigcount);
-        }else if(linestream.substr(0,6) == "#CHROM"){ printf("final");
+        if(linestream.substr(0,8) == "##contig"){
+	    parseHeader2(linestream,contig,++contigcount);
+        }else if(linestream.substr(0,2) == "##"){ 
+	    parseHeader1(linestream,headmap); 
+	}else if(linestream.substr(0,6) == "#CHROM"){ 
+            loadSampleNames(linestream,file);
 	    break; 
 	}else{
-	    parseHeader1(linestream,headmap1); 
+            cout << "ERROR: Failed to write the input file. Unknown format of header entry. \"" << "\"" << endl;
+	    cout << "REPORT: Failed to complete writing the data" << endl;
+	    return 1; /*finished reading header*/
 	}
     }
-    int count=headmap1.size();
-    header1=(META_1*)malloc(count*sizeof(META_1));
-    loadHeader1(headmap1,header1,file); 
-    free(header1);
-    free(header2);
+    loadHeader1(headmap,file); 
+    loadHeader2(contig,file,contigcount);
     H5Fclose(file);
     
 }
